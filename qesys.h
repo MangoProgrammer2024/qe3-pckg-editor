102f0438048e13f40211adea03047c4ef8daad7c
syntax-and-codestack <164366136+MangoProgrammer2024@users.noreply.github.com>
2025-01-07 13:17:17 -0600

    Update qesys.h

--- qesys.h
+++ qesys.h
@@ -2,6 +2,7 @@
 #define QESYS_H
 
 #include <string>
+#include <Windows.h>
 /*
 =======================
   QE GLOBAL VARIABLES
@@ -12,8 +13,24 @@
 =======================
 */
 void * q_eSysprintf(const char * _format, const char * text, ...);
-int * qSize_t( unsigned long * _size);
-typedef std::string qestring;
-char * q_eGlobalParse;
+typedef malloc qemalloc;
+typedef std::size_t qesize_t;
+
+typedef class QE_Global_GUI_Table{
+  public:
+ QE_Global_GUI_Table();
+  ~ QE_Global_GUI_Table();
+
+      HWND * QE_Camera_Widget;
+      HWND * QE_XY_Widget;
+      HWND * WINAPI_GL_Widget;
+      HWND * QE_Toolbar_Widget;
+      HWND * QE_Z_Widget;
+
+      virtual void WINAPI * QE_FlushApiWindows() = 0;
+
+        BOOLEAN QE_LoadWindows_Null(); 
+
+};
 
 #endif
